75a9e37cd2d16418eada4f31ca809f93e0278af5
fortueR <fmuchokore@gmail.com>
2024-10-10 19:44:08 +0200

    update script.js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,23 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "78b55t60fa4c079f70ao0031b0b0ceec";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} &unit=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Harare");
