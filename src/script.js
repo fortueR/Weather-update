4d1bf2d6964cd6dd75e216ac7d234ceb1ec8148f
fortueR <fmuchokore@gmail.com>
2024-10-11 19:57:48 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,12 +2,13 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 function searchCity(city) {
   let apiKey = "78b55t60fa4c079f70ao0031b0b0ceec";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} &unit=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(updateWeather);
 }
 function handleSearchSubmit(event) {
@@ -20,4 +21,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Harare");
+searchCity("Paris");
