b30e01e5fa8b7cf49073661d4d296b7bdab3d065
fortueR <fmuchokore@gmail.com>
2024-10-13 18:06:40 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -15,14 +15,10 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
   timeElement.innerHTML = formatDate(date);
-  iconElement.innerHTML = `<img
-                  src="${response.data.condition.icon_url}"
-                  class="weather-update-icon"
-                />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
-  let day = days[date.getDay()];
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
@@ -34,12 +30,13 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
+  let day = days[date.getDay()];
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
-  return `${day} ${hours} ${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
