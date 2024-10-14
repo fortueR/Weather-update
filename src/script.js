e5f7cc3cabf0862ef548f2938918ef5ede5d3210
fortueR <fmuchokore@gmail.com>
2024-10-14 19:01:40 +0200

    added javascript

--- src/script.js
+++ src/script.js
@@ -50,8 +50,32 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHtml = "";
 
+  let days = ["Tues", "Wed", "Thur", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+              <div class="weather-forecast-date">Frid</div>
+              <div class="weather-forecast-icon">🌤️</div>
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                  <strong>14ºC</strong>
+                </div>
+                <div class="weather-forecast-temperature">
+                  <strong>9ºC</strong>
+                </div>
+              </div>
+            </div>`;
+    forecastElement.innerHTML = forecastElement;
+  });
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+displayForecast();
